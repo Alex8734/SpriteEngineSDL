f8d003eb9ac394f506ec98b545359dc5154326e8
reschi <alexander@resch-web.at>
2024-09-26 09:01:44 +0200

    gitignore build

--- Engine/cmake-build-debug/ThirdParty/SDL2/cmake_uninstall.cmake
+++ Engine/cmake-build-debug/ThirdParty/SDL2/cmake_uninstall.cmake
@@ -1,10 +1,10 @@
 if (NOT EXISTS "C:/Users/alexa/source/CLionProjects/SpriteEngineSDL/Engine/cmake-build-debug/install_manifest.txt")
     message(FATAL_ERROR "Cannot find install manifest: \"C:/Users/alexa/source/CLionProjects/SpriteEngineSDL/Engine/cmake-build-debug/install_manifest.txt\"")
-endif(NOT EXISTS "C:/Users/alexa/source/CLionProjects/SpriteEngineSDL/Engine/cmake-build-debug/install_manifest.txt")
+endif()
 
 file(READ "C:/Users/alexa/source/CLionProjects/SpriteEngineSDL/Engine/cmake-build-debug/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
-foreach (file ${files})
+foreach(file ${files})
     message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
     execute_process(
         COMMAND C:/Users/alexa/AppData/Local/Programs/CLion/bin/cmake/win/x64/bin/cmake.exe -E remove "$ENV{DESTDIR}${file}"
@@ -14,5 +14,4 @@ foreach (file ${files})
     if(NOT ${rm_retval} EQUAL 0)
         message(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
     endif (NOT ${rm_retval} EQUAL 0)
-endforeach(file)
-
+endforeach()
